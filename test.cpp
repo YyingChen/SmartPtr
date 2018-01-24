1ad18412cd3f43f59fcd664a4ab384ed8026201a
YingChen <944963779@qq.com>
2018-01-24 13:10:28 +0800

    test

---
+++ test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+//#include "Auto_Ptr.h"
+//#include "Scoped_Ptr.h"
+#include"Shared_ptr.h"
+
+using namespace std;
+
+int main()
+{
+  // TestAutoPtr();
+	//TestScopedPtr();
+	TestSharedPtr();
+}
